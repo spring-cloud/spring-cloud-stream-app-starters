0c65b8e24c61cddc9d55a12e136d94c493a553bb
azwickey-pivotal <azwickey@Adams-MacBook-Pro.local>
2016-06-01 12:54:21 -0400

    added mongodb module

---
+++ mongodb/spring-cloud-starter-stream-source-mongodb/src/main/java/org/springframework/cloud/stream/app/mongodb/source/MongoDBSourceApplication.java
@@ -0,0 +1,18 @@
+package org.springframework.cloud.stream.app.mongodb.source;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class MongoDBSourceApplication {
+
+    protected final Logger LOG = LoggerFactory.getLogger(MongoDBSourceConfiguration.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDBSourceApplication.class, args);
+    }
+}
